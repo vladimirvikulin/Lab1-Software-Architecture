ea836aa8d1418efff94dafd0464fc6c1f84fa58c
danil2205 <04danil@gmail.com>
2023-02-26 21:34:52 +0200

    deleleted file main.go and comments in server.go

--- server.go
+++ server.go
@@ -6,10 +6,8 @@ import (
 )
 
 func main() {
-	// Register the handler function to handle all requests
 	http.HandleFunc("/", handler)
 
-	// Start the server on port 8795
 	fmt.Println("Listening on port 8795...")
 	http.ListenAndServe(":8795", nil)
 }
